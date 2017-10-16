f7b6bafd8978a335b6ad2df1133a9c674ee2a52a
Nimesh <nimesh17@Nimeshs-Macbook-Pro.local>
2017-10-17 03:00:26 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,62 @@
+const Alexa = require('alexa-sdk');
+var APP_ID = 'amzn1.ask.skill.ba2e1cf1-e8f6-4aa3-beec-e13dc607c971';
+exports.handler = function(event, context, callback){
+  var alexa = Alexa.handler(event, context, callback);
+  alexa.appId = APP_ID;
+  alexa.registerHandlers(handlers);
+  alexa.execute();
+};
+
+function wishTime(){
+  var dateIST = new Date();
+  var time = {}
+  time.hour = dateIST.getUTCHours + 5;
+  time.minutes = dateIST.getUTCMinutes + 30;
+  //date shifting for IST timezone +5 hours and 30 minutes
+  
+  if (time.hour < 12) return 'Good morning';
+  else if(time.hour < 18) return 'Good noon';
+  else return 'Good evening';
+}
+wishTime();
+var handlers={
+  'LaunchRequest': function(){
+    var speechOutput = 'Welcome to Wish. Using this skill you can wish your guests. Whom you want me to wish';
+    var reprompt = 'for example, You can say say hello to Nimesh';
+    this.emit(':ask', speechOutput);
+  },
+  'wisherIntent': function(){
+     let name = this.event.request.intent.slots.guestName.value;
+     let speechOutput = 'Hello ' + name + ". " + wishTime();
+     this.emit(':tell', speechOutput); 
+  },
+  'welcomeIntent': function(){
+    let name = this.event.request.intent.slots.guestName.value;
+    let speechOutput = 'Welcome to our sweet home ' + name + '!';
+    this.emit(':tell', speechOutput); 
+  },
+  'AMAZON.StopIntent'() {
+    this.response.speak('Bye');
+    this.emit(':responseReady');
+  },
+
+  'AMAZON.HelpIntent'() {
+    this.response.speak(
+      `You can try: 'alexa, open ${process.env.SKILL_NAME}'` +
+        ` or 'alexa, ask ${process.env.SKILL_NAME} Whom you want me to wish'`
+    );
+    this.emit(':responseReady');
+  },
+
+  'AMAZON.CancelIntent'() {
+    this.response.speak('Bye');
+    this.emit(':responseReady');
+  },
+
+  Unhandled() {
+    this.response.speak(
+      `Sorry, I didn't get that. You can try: 'alexa, ${process.env.SKILL_NAME}'` +
+        ` or 'alexa, ask ${process.env.SKILL_NAME} Whom you want me to wish'`
+    );
+  }
+};
